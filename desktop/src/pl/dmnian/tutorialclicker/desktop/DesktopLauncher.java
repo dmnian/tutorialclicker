6babf082bf197e5a7a4efd65fed7d036a56d43c4
dmnian <dmnian@gmail.com>
2016-07-26 16:23:19 +0200

    init

---
+++ desktop/src/pl/dmnian/tutorialclicker/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.dmnian.tutorialclicker.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.dmnian.tutorialclicker.TutorialClickerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TutorialClickerGame(), config);
+	}
+}
