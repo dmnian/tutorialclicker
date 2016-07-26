b6b81e565370c4736806ab375110f57c71c251a3
dmnian <dmnian@gmail.com>
2016-07-26 17:40:07 +0200

    config desktop launcher

--- core/src/pl/dmnian/tutorialclicker/TutorialClickerGame.java
+++ core/src/pl/dmnian/tutorialclicker/TutorialClickerGame.java
@@ -1,33 +1,36 @@
 package pl.dmnian.tutorialclicker;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import pl.dmnian.tutorialclicker.screens.SplashScreen;
 
-public class TutorialClickerGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class TutorialClickerGame extends Game {
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    public final static String GAME_NAME = "Tutorial Clicker";
+
+    public final static int WIDTH = 480;
+    public final static int HEIGHT = 700;
+
+    private boolean  paused;
+
+    @Override
+    public void create() {
+        this.setScreen(new SplashScreen(this));
+    }
+
+    /**
+     * --------------
+     * getters and setters
+     */
+
+    public boolean isPaused() {
+        return paused;
+    }
+
+    public void setPaused(boolean paused) {
+        this.paused = paused;
+    }
 }
--- desktop/src/pl/dmnian/tutorialclicker/desktop/DesktopLauncher.java
+++ desktop/src/pl/dmnian/tutorialclicker/desktop/DesktopLauncher.java
@@ -5,8 +5,15 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import pl.dmnian.tutorialclicker.TutorialClickerGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new TutorialClickerGame(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+        config.title = TutorialClickerGame.GAME_NAME;
+        config.width = TutorialClickerGame.WIDTH;
+        config.height = TutorialClickerGame.HEIGHT;
+        config.resizable = false;
+
+        new LwjglApplication(new TutorialClickerGame(), config);
+
+    }
 }
